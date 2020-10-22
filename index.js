377d3386fb33b563503b879eebdf4cadaf19ffd5
Donkada Ashok kumar <51916@hexaware.com>
2020-10-22 17:00:54 +0530

    api transactionpool and maintransactionpool added

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 const PubSub = require('./app/pubsub');
+const Transaction = require('./wallet/transaction');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -22,6 +27,15 @@ app.post('/api/mine', (req, res) => {
     blockchain.addBlock({ data });
     pubsub.broadcastChain()
     res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+    const transaction = wallet.createTransaction({ recipient, amount });
+    transactionPool.setTransaction(transaction);
+
+    console.log('transactionPool', transactionPool);
+    res.json({ transaction });
 });
 
 const syncChains = () => {
