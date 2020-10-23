ad6e0bb226c01c16b7b4e91089598743f9926d9c
Donkada Ashok kumar <51916@hexaware.com>
2020-10-23 17:21:27 +0530

    validated input balances

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,6 @@
 const Block = require("./block");
 const { cryptoHash } = require("../util");
+const Wallet = require('../wallet');
 const Transaction = require('../wallet/transaction');
 const { REWARD_INPUT, MINING_REWARD } = require("../config");
 
@@ -53,6 +54,16 @@ class Blockchain {
                 } else {
                     if (!Transaction.validTransaction(transaction)) {
                         console.error('Invalid transaction');
+                        return false;
+                    }
+
+                    const trueBalance = Wallet.calculateBalance({
+                        chain: this.chain,
+                        address: transaction.input.address
+                    });
+
+                    if (transaction.input.amount !== trueBalance) {
+                        console.error('Invalid input amount');
                         return false;
                     }
                 }
