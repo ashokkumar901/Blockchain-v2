63455f78648a0b2d737a1681df3e653d3de7fec3
Donkada Ashok kumar <51916@hexaware.com>
2020-10-23 14:01:47 +0530

    calculateBalance method added

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,6 @@
 const Transaction = require('./transaction');
 const { STARTING_BALANCE } = require('../config');
-const { ec, cryptoHash } = require('../util'); 
+const { ec, cryptoHash } = require('../util');
 
 class Wallet {
     constructor() {
@@ -21,6 +21,24 @@ class Wallet {
         }
 
         return new Transaction({ senderWallet: this, recipient, amount });
+    }
+
+    static calculateBalance({ chain, address }) {
+        let outputsTotal = 0;
+
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+
+            for (let transaction of block.data) {
+                const addressOutput = transaction.outputMap[address];
+
+                if (addressOutput) {
+                    outputsTotal += addressOutput
+                }
+            }
+        }
+
+        return STARTING_BALANCE + outputsTotal;
     }
 }
 
