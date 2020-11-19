9dc719be275aee733b21139ae80b9348acfe8d42
Donkada Ashok kumar <51916@hexaware.com>
2020-11-19 11:30:43 +0530

    front end development work flow added

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<div>Cryptochain in React!</div>,
+render(<div>Cryptochain in React!  second Live update</div>,
     document.getElementById('root')
 );
